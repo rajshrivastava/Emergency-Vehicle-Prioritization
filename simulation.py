e53fbae82f654636c555e6f6819f049b75acb3cc
mdighe <mdighe@ncsu.edu>
2019-12-09 21:48:22 -0500

    Refactor initial code

--- simulation.py
+++ simulation.py
@@ -3,56 +3,10 @@ import pygame
 from math import sin, radians, degrees, copysign
 from pygame.math import Vector2
 import random 
-    
-class Background(pygame.sprite.Sprite):
-    def __init__(self, image_file, location):
-        pygame.sprite.Sprite.__init__(self)  #call Sprite initializer
-        self.image = pygame.image.load(image_file)
-        self.image = pygame.transform.scale(self.image, (1280, 720))
-        self.rect = self.image.get_rect()
-        self.rect.left, self.rect.top = location
+from car import Car
+from background import Background
 
-class Car:
-    def __init__(self, x, y, angle=0.0, length=4, max_steering=30, max_acceleration=4.0):
-        self.position = Vector2(x, y)
-        self.velocity = Vector2(0.0, 0.0)
-        self.angle = angle
-        self.length = length
-        self.max_acceleration = max_acceleration
-        self.max_steering = max_steering
-        self.max_velocity = 3
-        #self.brake_deceleration = 5
-        self.brake_deceleration = 1
-        
-        self.free_deceleration = 1
-
-        self.acceleration = 0.0
-        self.steering = 0.0
-        self.counter = 0
-
-    def update(self, dt):
-        self.velocity += (self.acceleration * dt, 0)
-        self.velocity.x = max(-self.max_velocity, min(self.velocity.x, self.max_velocity))
-
-        if self.steering:
-            turning_radius = self.length / sin(radians(self.steering))
-            angular_velocity = self.velocity.x / turning_radius
-        else:
-            angular_velocity = 0
-
-        self.position += self.velocity.rotate(-self.angle) * dt
-        
-        if not self.angle:          #moving right
-            if self.position[0] > 42:
-                self.position[0] = 0
-        else:                       #moving left
-            if self.position[0] < -10:
-                self.position[0] = 42
-                
-        self.angle += degrees(angular_velocity) * dt
-
-
-class Game:
+class Simulation:
     def __init__(self):
         pygame.init()
         pygame.display.set_caption("Traffic simulation")
@@ -321,5 +275,5 @@ class Game:
 
 
 if __name__ == '__main__':
-    game = Game()
+    game = Simulation()
     game.run()
